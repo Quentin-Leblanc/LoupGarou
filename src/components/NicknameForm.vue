83a1e003b3ea31e89f99401f691f666dd93b3c0c
Quentin-Leblanc <artofleb@gmail.com>
2022-07-16 19:37:22 +0200

    ajout du chemin des assets
    css/img/fonts

---
+++ src/components/NicknameForm.vue
@@ -0,0 +1,68 @@
+
+<script>
+
+  export default {
+      name: "NicknameForm",
+    
+      data() {
+        return {
+          form: {
+            pseudo: null,
+            error: false,
+          }
+        };
+      },
+      methods: {
+        checkPseudo() {
+          const valideChars = /^[a-zA-Z][0-9a-zA-Z .,'-]*$/i;
+          if (
+            this.form.pseudo == null ||
+            this.form.pseudo == "" ||
+            this.form.pseudo.length >= 20 ||
+            !valideChars.test(this.form.pseudo)
+          ) {
+            this.form.error = true;
+          } else {
+            console.log("Votre pseudo est ", this.form.pseudo);
+            this.onFormSuccess(this.form.pseudo);
+            
+          }
+        },
+      },
+      props: ['onFormSuccess']
+    };
+  </script>
+
+<template >
+          <div class="input-group">
+            <!-- pseudo -->
+            <input
+              v-model="form.pseudo"
+              placeholder="Entrez votre pseudo"
+              @keyup.enter="checkPseudo"
+              type="text"
+              class="form-control"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+
+            <!-- submit -->
+            <div class="input-group-append">
+              <button
+                v-on:click="checkPseudo"
+                class="btn btn-dark go"
+                type="button"
+              >
+                Jouer
+              </button>
+            </div>
+          </div>
+          <div
+            v-if="form.error == true"
+            class="alert alert-danger"
+            role="alert"
+          >
+            Pseudo incorrect
+          </div>
+</template>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+
+
+import "./assets/css/custom.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-
 createApp(App).mount("#app");
+
+
