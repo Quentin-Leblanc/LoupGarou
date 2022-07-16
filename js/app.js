3e61d4f4a57505a44b27d26fd7f0a41910b02f0c
Quentin-Leblanc <artofleb@gmail.com>
2022-07-16 10:33:10 +0200

    Changements steps

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ createApp({
 
     data() {
         return {
-            step1 :  true,
+            step :  1,
             message: "Bienvenue les p'tits loups",
             form: {
                 pseudo : null,
@@ -16,18 +16,18 @@ createApp({
     },
     methods: {
         checkPseudo(){
-            const valideChars =  /^[a-zA-Z][0-9a-zA-Z .,'-]*$/i ;   
+            const valideChars = /^[a-zA-Z][0-9a-zA-Z .,'-]*$/i ;   
             if(this.form.pseudo == null || this.form.pseudo == "" || this.form.pseudo.length >= 20 || !valideChars.test(this.form.pseudo)){
                 this.form.error = true
             }  else {
-            this.step1 = false
+            this.step = 2
             console.log("Votre pseudo est ", this.form.pseudo)
             setTimeout(() => {
                 this.isFaded = true;
               }, 250);
             setTimeout(() => {
             this.outFaded = true;
-            }, 800);
+            }, 700);
             }
         }
     }
