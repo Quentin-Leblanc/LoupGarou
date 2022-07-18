9e9b1a9f85565d624fc35d344c777039f2635039
Quentin-Leblanc <artofleb@gmail.com>
2022-07-18 14:52:52 +0200

    ajout de firebase db

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-
-
 import "./assets/css/custom.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-
-import router from './router'; 
+import router from "./router";
+/* eslint-disable */
+import firebase from "./firebase";
 
 createApp(App).use(router).mount("#app");
-
-
